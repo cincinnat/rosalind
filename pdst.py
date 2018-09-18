0816e9933b0ea5e76a6cf01e8660bd8264dfd71a
cincinnatus <c.cincinnat@gmail.com>
2018-09-18 22:49:12 +0300

    solve PDST

---
+++ pdst.py
@@ -0,0 +1,35 @@
+#! /usr/bin/python3
+
+import sys
+import argparse
+import signal
+import numpy as np
+
+import tools
+
+
+def main(args):
+    inputs = tools.io.read_fasta(sys.stdin)
+    strings = [''.join(seq) for _, seq in inputs]
+
+    def foo(i, j):
+        dist = tools.helpers.hamming_distance(strings[i], strings[j])
+        return dist / len(strings[i])
+
+    dist = np.fromfunction(np.vectorize(foo), (len(strings), len(strings)), dtype=int)
+    for i in range(len(strings)):
+        print(*dist[i, :])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='description',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    args = parser.parse_args()
+
+    try:
+        main(args)
+    except BrokenPipeError:
+        sys.exit(128 + signal.SIGPIPE)
+    except KeyboardInterrupt:
+        sys.exit(128 + signal.SIGINT)
