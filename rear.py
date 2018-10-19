738911638db878cb0cb0fb06ead49404b5f62ce0
cincinnatus <c.cincinnat@gmail.com>
2018-10-19 10:48:32 +0300

    move `reversal_distance()` to tools.dist

--- rear.py
+++ rear.py
@@ -9,62 +9,11 @@ import math
 import tools
 
 
-def neighbors(seq, i, j):
-    return abs(seq[i] - seq[j]) == 1
-
-
-def find_breaks(seq):
-    seq = [min(seq)-1] + seq + [max(seq)+1]
-    breaks = []
-    for i in range(len(seq)-1):
-        if not neighbors(seq, i, i+1):
-            breaks.append(i)
-    return breaks
-
-
-def flip(seq, i, j):
-    return seq[0:i] + seq[i:j][::-1] + seq[j:len(seq)]
-
-
-def _reversal_distance(seq, breaks):
-    if len(breaks) == 0:
-        return 0
-    if len(breaks) == 2:
-        return 1
-    if len(breaks) == 3:
-        return 2
-
-    # there is always > 1 break
-    assert len(breaks) != 1, (seq, breaks)
-
-    dist = float('inf')
-
-    for i in breaks:
-        for j in breaks[i+1:]:
-            new_seq = flip(seq, i, j)
-            new_breaks = find_breaks(new_seq)
-            if len(new_breaks) < len(breaks):
-                # plus one flip
-                d = 1 + _reversal_distance(new_seq, new_breaks)
-                dist = min(d, dist)
-    return dist
-
-
-def reversal_distance(a, b):
-    # map `a` into sequence [0 .. len(a)-1]
-    code_table = dict(zip(a, range(len(a))))
-    b = list(map(code_table.get, b))
-
-    breaks = find_breaks(b)
-
-    return _reversal_distance(b, breaks)
-
-
 def main(args):
     inputs = filter(bool, map(str.split, sys.stdin))
     inputs = map(lambda l: list(map(int, l)), inputs)
     pairs = tools.helpers.iter_chunks(inputs, chunk_size=2)
-    distances = itertools.starmap(reversal_distance, pairs)
+    distances = itertools.starmap(tools.dist.reversal_distance, pairs)
 
     print(*distances)
 
--- tools/dist.py
+++ tools/dist.py
@@ -1,6 +1,13 @@
 import collections
 import itertools
 
+__all__ = [
+    'hamming_distance',
+    'edit_matrix',
+    'edit_dist',
+    'reversal_distance',
+]
+
 
 def hamming_distance(s1, s2):
     return sum((a != b for a, b in zip(s1, s2)))
@@ -62,3 +69,53 @@ def edit_matrix(s1, s2, indel_weight=1, substitution_weight=1):
 def edit_dist(s1, s2, indel_weight=1, substitution_weight=1):
     dist, _ = edit_matrix(s1, s2, indel_weight, substitution_weight)
     return dist
+
+
+def _find_breaks(seq):
+    def neighbors(seq, i, j):
+        return abs(seq[i] - seq[j]) == 1
+
+    seq = [min(seq)-1] + seq + [max(seq)+1]
+    breaks = []
+    for i in range(len(seq)-1):
+        if not neighbors(seq, i, i+1):
+            breaks.append(i)
+    return breaks
+
+
+def _flip(seq, i, j):
+    return seq[0:i] + seq[i:j][::-1] + seq[j:len(seq)]
+
+
+def _reversal_distance(seq, breaks):
+    if len(breaks) == 0:
+        return 0
+    if len(breaks) == 2:
+        return 1
+    if len(breaks) == 3:
+        return 2
+
+    # there is always > 1 break
+    assert len(breaks) != 1, (seq, breaks)
+
+    dist = float('inf')
+
+    for i in breaks:
+        for j in breaks[i+1:]:
+            new_seq = _flip(seq, i, j)
+            new_breaks = _find_breaks(new_seq)
+            if len(new_breaks) < len(breaks):
+                # plus one flip
+                d = 1 + _reversal_distance(new_seq, new_breaks)
+                dist = min(d, dist)
+    return dist
+
+
+def reversal_distance(a, b):
+    # map `a` into sequence [0 .. len(a)-1]
+    code_table = dict(zip(a, range(len(a))))
+    b = list(map(code_table.get, b))
+
+    breaks = _find_breaks(b)
+
+    return _reversal_distance(b, breaks)
